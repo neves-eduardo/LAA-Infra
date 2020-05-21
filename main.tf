f4d3be50ae31eb5a1e000d192a861010f08e6b5e
neves-eduardo <eduardoneves858@gmail.com>
2020-05-21 10:52:34 -0300

    feature: adding loadbalancer and dns to main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ data "aws_ami" "laa" {
   owners = ["self"]
 
   filter {
-  name   = "name"
-  values = ["laa"]
+    name   = "name"
+    values = ["laa"]
   }
 }
 
@@ -13,13 +13,13 @@ data "aws_ami" "laa-influxdb" {
   owners = ["self"]
 
   filter {
-  name   = "name"
-  values = ["laa-influxdb"]
+    name   = "name"
+    values = ["laa-influxdb"]
   }
 }
 
 resource "aws_security_group" "security-laa" {
-  name        = "security-laa"
+  name = "security-laa"
 
   ingress {
     from_port   = 22
@@ -32,7 +32,7 @@ resource "aws_security_group" "security-laa" {
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }  
+  }
   ingress {
     from_port   = 8086
     to_port     = 8086
@@ -42,24 +42,51 @@ resource "aws_security_group" "security-laa" {
 }
 
 resource "aws_instance" "laa" {
-    instance_type = "t2.micro"
-    ami = data.aws_ami.laa.id
-    security_groups = [aws_security_group.security-laa.name]
-    key_name = "laachallenge1"
-      tags = {
+  instance_type   = "t2.micro"
+  ami             = data.aws_ami.laa.id
+  security_groups = [aws_security_group.security-laa.name]
+  key_name        = "laachallenge1"
+  tags = {
     Name = "laa"
   }
 }
 
 resource "aws_instance" "laa-influxdb" {
-    instance_type = "t2.micro"
-    ami = data.aws_ami.laa-influxdb.id
-    security_groups = [aws_security_group.security-laa.name]
-    key_name = "laachallenge1"
-      tags = {
+  instance_type   = "t2.micro"
+  ami             = data.aws_ami.laa-influxdb.id
+  security_groups = [aws_security_group.security-laa.name]
+  key_name        = "laachallenge1"
+  tags = {
     Name = "laa-influxdb"
   }
-}
+  }
+  resource "aws_elb" "elb-laa" {
+    name               = "elb-laa"
+    availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+    security_groups    = ["${aws_security_group.security-laa.id}"]
+
+    listener {
+      instance_port     = 8080
+      instance_protocol = "http"
+      lb_port           = 8080
+      lb_protocol       = "http"
+    }
+    listener {
+      instance_port     = 8086
+      instance_protocol = "http"
+      lb_port           = 8086
+      lb_protocol       = "http"
+    }
+
+
+  }
+
+  output "dns-elb" {
+    value = "${aws_elb.elb-laa.dns_name}"
+  }
+
+
+
 
 
 
