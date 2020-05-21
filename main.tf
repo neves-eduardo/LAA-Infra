22070b458e0d654244b3d99d823cee7854b9626b
neves-eduardo <eduardoneves858@gmail.com>
2020-05-21 11:50:23 -0300

    refactor: fixing security group

--- main.tf
+++ main.tf
@@ -33,6 +33,20 @@ resource "aws_security_group" "security-laa" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 8080
+    to_port     = 8086
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    egress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 8086
     to_port     = 8086
